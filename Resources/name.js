b55acfca7874e0cfee67a8284fa7247cbee9087b
Mike Jensen <michael.jensen@hudl.com>
2014-07-29 10:22:31 -0500

    Added jQuery functionality
    Keyboard dismissal, name retrieval

---
+++ Resources/name.js
@@ -0,0 +1,14 @@
+function getName() {
+	if (event.keyCode==13) {
+		var name = document.getElementById("name").value;
+
+		$('#name').blur();
+
+		//alert(name);
+	}
+	else {
+		setTimeout(function() {
+			$('#name').blur();
+		}, 20000);
+	}
+}
